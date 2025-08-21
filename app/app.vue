823231908f0ec9f6116d679cba2a35c3ed7fe667
Alexey <aleksey.aleksandrovich.mc@gmail.com>
2025-08-21 19:30:53 +0300

    add new components style

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import "@/app/assets/styles/main.scss";
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
---
+++ app/components/MyUI/MyBtn/MyBtn.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    <p>Template </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
